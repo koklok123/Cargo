71223f67b799855486cee7b87ad9dd3c6a349c0d
koklok <lokgom774@gmail.com>
2025-01-04 22:43:34 +0600

    Небольшие изменения

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.test import TestCase
+from django.urls import reverse
 # Create your tests here.
+
+class MyViewTestCase(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'index.html')
